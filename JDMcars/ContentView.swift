7019b876b161acf78f9972ac81d17359fb1ae210
Damla KS <damlaksrbst@gmail.com>
2023-07-16 01:46:50 +0300

    first commit

--- JDMcars/ContentView.swift
+++ JDMcars/ContentView.swift
@@ -9,18 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            
+            List {
+                ForEach(cars) { car in
+                    Section(header: Text(car.title)) {
+                        ForEach(car.elements) { element in
+                            NavigationLink(destination: DetailsView(chosenElement: element)) {
+                                Text(element.name)
+                            }
+                        }
+                    }
+                }
+            }.navigationTitle(Text("JDM Cars"))
+            
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
